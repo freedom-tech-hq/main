35e32747e80314a3327b72dd5c0a06215f5dab48
Pavel Koryagin <pavel.koryagin@freedomtechhq.com>
2025-04-15 22:06:41 +0200

    Add Reverse DNS for new IPs

--- code/iac/configurations/account/main.tf
+++ code/iac/configurations/account/main.tf
@@ -5,8 +5,21 @@ provider "google" {
 
 # Create a DNS zone for dev.linefeedr.com
 resource "google_dns_managed_zone" "dev_linefeedr_com" {
-  name        = "dev-linefeedr-com"
-  dns_name    = "dev.linefeedr.com."
+  name     = "dev-linefeedr-com"
+  dns_name = "dev.linefeedr.com."
+
+  dnssec_config {
+    state = "on"
+  }
+}
+
+# TODO: The whole zone should be in the production account
+# a sub-zone e.g. 82.26.157.128/25 should be manageable by the dev account if possible (not critical - every PTR be in production)
+# Create a reverse DNS zone for 82.26.157.0/24
+resource "google_dns_managed_zone" "rdns_82_26_157" {
+  name        = "rdns-82-26-157"
+  dns_name    = "157.26.82.in-addr.arpa."
+  description = "Reverse DNS zone for 82.26.157.0/24"
 
   dnssec_config {
     state = "on"
@@ -17,4 +30,10 @@ resource "google_dns_managed_zone" "dev_linefeedr_com" {
 output "dev_linefeedr_com_name_servers" {
   description = "Name servers for dev.linefeedr.com zone"
   value       = google_dns_managed_zone.dev_linefeedr_com.name_servers
+}
+
+# Output the reverse DNS zone name servers
+output "rdns_82_26_157_name_servers" {
+  description = "Name servers for 82.26.157.0/24 reverse DNS zone"
+  value       = google_dns_managed_zone.rdns_82_26_157.name_servers
 }
