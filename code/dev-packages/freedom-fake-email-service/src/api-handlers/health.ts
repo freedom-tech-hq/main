b78857fd79f8943a898ed8c102550af9f20dcdd2
Brian Westphal <brian@freedomtechhq.com>
2025-04-24 09:12:02 +0800

    Fixed a lint issue

--- code/dev-packages/freedom-fake-email-service/src/api-handlers/health.ts
+++ code/dev-packages/freedom-fake-email-service/src/api-handlers/health.ts
@@ -4,6 +4,6 @@ import { makeHttpApiHandler } from 'freedom-server-api-handling';
 
 import { startedAt, version } from '../version.ts';
 
-export default makeHttpApiHandler([import.meta.filename], { api: api.health.GET }, async (trace) =>
+export default makeHttpApiHandler([import.meta.filename], { api: api.health.GET }, async () =>
   makeSuccess({ body: { version, startedAt: new Date(startedAt), healthy: true } })
 );
