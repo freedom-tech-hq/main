5154461a1e9381e0969deffc263b8370b4c402ba
Brian Westphal <brian@freedomtechhq.com>
2025-04-28 09:28:59 +0800

    Fixed email from field more generally - On the summary list, it now wraps when there's multiple from names or a long name and ellipsizes when it's too wide - On the detail list, it now wraps and ellipsizes names and wraps email addresses anywhere if needed - From groups are now better supported as well
    Still, all of this should be considered placeholder UI until we have a more comprehensive app design.
    
    https://github.com/freedom-tech-hq/main/issues/66

--- code/apps/freedom-email-app/src/modules/mail-thread/components/MailListItem.tsx
+++ code/apps/freedom-email-app/src/modules/mail-thread/components/MailListItem.tsx
@@ -1,7 +1,10 @@
 import type { ListItemTextSlotsAndSlotProps, SxProps, Theme } from '@mui/material';
 import { Avatar, Divider, ListItem, ListItemAvatar, ListItemText, Paper, Stack, Typography } from '@mui/material';
+import { parseFrom } from 'email-addresses';
 import { LOCALIZE } from 'freedom-localization';
 import { useT } from 'freedom-react-localization';
+import type { CSSProperties } from 'react';
+import { Fragment } from 'react/jsx-runtime';
 
 import { formatDate } from '../../../utils/formatDate.ts';
 import { formatTime } from '../../../utils/formatTime.ts';
@@ -20,6 +23,61 @@ export interface MailListItemProps<TagT> extends Omit<MailDataSourceMailItem, 't
 export const MailListItem = <TagT,>({ isFirst, mail }: MailListItemProps<TagT>) => {
   const t = useT();
 
+  const parsedFrom = parseFrom(mail.from) ?? [];
+  const fromTags = parsedFrom.map((parsed, index) => {
+    switch (parsed.type) {
+      case 'group':
+        return (
+          <Fragment key={index}>
+            <Typography fontWeight="bold" title={parsed.name} sx={nameStyle}>
+              {parsed.name}
+            </Typography>{' '}
+            <Typography
+              component="span"
+              color="textSecondary"
+              sx={addressStyle}
+              title={parsed.addresses.map((from) => from.address).join(', ')}
+            >
+              &lt;
+              {parsed.addresses.map((member, index) => (
+                <Fragment key={index}>
+                  <span style={wrapTextAnywhereReactStyle}>{member.local}</span>@
+                  <span style={wrapTextAnywhereReactStyle}>{member.domain}</span>
+                  {index < parsed.addresses.length - 1 ? <span style={commaReactStyle}>,</span> : null}
+                </Fragment>
+              ))}
+              &gt;
+            </Typography>
+          </Fragment>
+        );
+
+      case 'mailbox':
+        if (parsed.name === null) {
+          return (
+            <Typography key={index} component="span" fontWeight="bold" title={parsed.address}>
+              <span style={wrapTextAnywhereReactStyle}>{parsed.local}</span>@<span style={wrapTextAnywhereReactStyle}>{parsed.domain}</span>
+              {index < parsedFrom.length - 1 ? <span style={commaReactStyle}>,</span> : null}
+            </Typography>
+          );
+        } else {
+          return (
+            <Fragment key={index}>
+              <Typography fontWeight="bold" title={parsed.name} sx={nameStyle}>
+                {parsed.name}
+              </Typography>{' '}
+              <Typography component="span" color="textSecondary" sx={addressStyle} title={parsed.address}>
+                &lt;
+                <span style={wrapTextAnywhereReactStyle}>{parsed.local}</span>@
+                <span style={wrapTextAnywhereReactStyle}>{parsed.domain}</span>
+                &gt;
+                {index < parsedFrom.length - 1 ? <span style={commaReactStyle}>,</span> : null}
+              </Typography>
+            </Fragment>
+          );
+        }
+    }
+  });
+
   return (
     <Paper elevation={4} sx={{ position: 'relative', mx: 3, mt: isFirst ? 0 : 3, mb: 3 }}>
       <MailActionsMenu />
@@ -31,11 +89,8 @@ export const MailListItem = <TagT,>({ isFirst, mail }: MailListItemProps<TagT>) 
             </Stack>
           </ListItemAvatar>
           <Stack alignItems="stretch" sx={headerContentStyle}>
-            <Stack direction="row" justifyContent="space-between" gap={1}>
-              <Typography fontWeight="bold" sx={ellipsizeStyle}>
-                {mail.from}
-              </Typography>
-              <Stack direction="row" gap={1}>
+            <div>
+              <Stack direction="row" gap={1} sx={{ float: 'right', ml: 1 }}>
                 <Typography variant="body2" color="textSecondary" sx={noWrapStyle}>
                   {formatDate(mail.timeMSec)}
                 </Typography>
@@ -43,10 +98,11 @@ export const MailListItem = <TagT,>({ isFirst, mail }: MailListItemProps<TagT>) 
                   {formatTime(mail.timeMSec)}
                 </Typography>
               </Stack>
-            </Stack>
+              <div>{fromTags}</div>
+            </div>
             <Stack direction="row" gap={1}>
               <Typography variant="body2">{$toLabel(t)}</Typography>
-              <Typography variant="body2" color="textSecondary" sx={ellipsizeStyle}>
+              <Typography variant="body2" color="textSecondary" sx={wrapTextAnywhereStyle}>
                 {mail.to}
               </Typography>
             </Stack>
@@ -68,7 +124,23 @@ export const MailListItem = <TagT,>({ isFirst, mail }: MailListItemProps<TagT>) 
 // Helpers
 
 const avatarStyle: SxProps<Theme> = { minWidth: '48px' };
-const ellipsizeStyle: SxProps<Theme> = { whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' };
+const nameStyle: SxProps<Theme> = {
+  display: 'inline-block',
+  maxWidth: '100%',
+  lineHeight: 1,
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis'
+};
+const addressStyle: SxProps<Theme> = {
+  display: 'inline-block',
+  maxWidth: '100%',
+  lineHeight: 1,
+  overflow: 'hidden'
+};
+const commaReactStyle: CSSProperties = { marginRight: '0.25em' };
+const wrapTextAnywhereStyle: SxProps<Theme> = { overflowWrap: 'anywhere' };
+const wrapTextAnywhereReactStyle: CSSProperties = { overflowWrap: 'anywhere' };
 const headerContentStyle: SxProps<Theme> = { flexGrow: 1, overflow: 'hidden' };
 const noWrapStyle: SxProps<Theme> = { whiteSpace: 'nowrap' };
 const subjectSlotProps: ListItemTextSlotsAndSlotProps['slotProps'] = { primary: { fontWeight: 'bold' } };
