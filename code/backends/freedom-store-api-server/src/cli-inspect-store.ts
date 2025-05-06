69d457b1ddf962c6443fbb7723e2202be94bfc8e
Pavel Koryagin <pavel.koryagin@freedomtechhq.com>
2025-05-06 19:55:49 +0200

    stash

--- code/backends/freedom-store-api-server/src/cli-inspect-store.ts
+++ code/backends/freedom-store-api-server/src/cli-inspect-store.ts
@@ -2,8 +2,10 @@ import { stdin as input, stdout as output } from 'node:process';
 import readline from 'node:readline/promises';
 
 import { makeAsyncResultFunc, makeSuccess } from 'freedom-async';
+import { type Base64String } from 'freedom-basic-data';
 import { buildMode, makeTrace } from 'freedom-contexts';
-import { getMailAgentUserKeys } from 'freedom-db';
+// import { getMailAgentUserKeys } from 'freedom-db';
+import { decryptEmailCredentialWithPassword, type EmailCredential } from 'freedom-email-user';
 
 let expectedBuildMode = 'PROD' as 'DEV' | 'PROD';
 DEV: expectedBuildMode = 'DEV';
@@ -14,35 +16,42 @@ if (expectedBuildMode !== buildMode) {
 const main = makeAsyncResultFunc(
   [import.meta.filename],
   async (trace) => {
+    let creds: EmailCredential;
+
     // Input
     const rl = readline.createInterface({ input, output });
     try {
-      const credInput = await rl.question('Paste private credentials as JSON (leave blank to use server keys): ');
-      let creds;
-      if (credInput.trim()) {
-        try {
-          creds = JSON.parse(credInput);
-          console.log('Parsed credentials:', creds);
-        } catch (err) {
-          console.error('Failed to parse JSON:', err);
-          process.exit(1);
-        }
-      } else {
-        const result = await getMailAgentUserKeys(trace);
+      const credInput = (await rl.question('Paste private credentials as JSON (leave blank to use server keys): ')).trim();
+      if (credInput !== '') {
+        const password = await rl.question('Enter password for credential: ');
+
+        const result = await decryptEmailCredentialWithPassword(trace, {
+          encryptedEmailCredential: credInput as Base64String,
+          password
+        });
         if (!result.ok) {
-          console.error('Failed to get server keys:', result.value);
+          console.error('Failed to decrypt credential:', result.value);
           process.exit(1);
         }
+
         creds = result.value;
-        console.log('Loaded server keys:', creds);
+      } else {
+        // TODO:
+        // const result = await getMailAgentUserKeys(trace);
+        // if (!result.ok) {
+        //   console.error('Failed to get server keys:', result.value);
+        //   process.exit(1);
+        // }
+        // creds = result.value;
+        // console.log('Loaded server keys:', creds);
+        throw new Error('Not implemented');
       }
     } finally {
       rl.close();
     }
 
     // Placeholder for next step logic
-    console.log('cli-inspect-store main() executed');
-
+    console.log('cli-inspect-store main() executed', creds);
     return makeSuccess(undefined);
   },
   {
