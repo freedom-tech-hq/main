9c9440a4ef78829e11ae213909ca0a2c9e5f5f13
Pavel Koryagin <pavel.koryagin@freedomtechhq.com>
2025-05-06 20:09:54 +0200

    stash

--- code/backends/freedom-store-api-server/src/cli-inspect-store.ts
+++ code/backends/freedom-store-api-server/src/cli-inspect-store.ts
@@ -1,11 +1,20 @@
 import { stdin as input, stdout as output } from 'node:process';
 import readline from 'node:readline/promises';
 
-import { makeAsyncResultFunc, makeSuccess } from 'freedom-async';
+import { makeAsyncResultFunc, makeFailure, makeSuccess, type PR, uncheckedResult } from 'freedom-async';
 import { type Base64String } from 'freedom-basic-data';
+import { NotFoundError } from 'freedom-common-errors';
 import { buildMode, makeTrace } from 'freedom-contexts';
+import type { CryptoKeySetId, PrivateCombinationCryptoKeySet } from 'freedom-crypto-data';
+import type { UserKeys } from 'freedom-crypto-service';
 // import { getMailAgentUserKeys } from 'freedom-db';
-import { decryptEmailCredentialWithPassword, type EmailCredential } from 'freedom-email-user';
+import { decryptEmailCredentialWithPassword } from 'freedom-email-user';
+import { storageRootIdInfo } from 'freedom-sync-types';
+import { prettyStoreLs } from 'freedom-syncable-store/tests';
+import { getServerSyncableStore } from 'freedom-syncable-store-server';
+import type { MutableSyncableStore } from 'freedom-syncable-store-types';
+
+import { initApp } from './initApp.ts';
 
 let expectedBuildMode = 'PROD' as 'DEV' | 'PROD';
 DEV: expectedBuildMode = 'DEV';
@@ -16,7 +25,10 @@ if (expectedBuildMode !== buildMode) {
 const main = makeAsyncResultFunc(
   [import.meta.filename],
   async (trace) => {
-    let creds: EmailCredential;
+    let store: MutableSyncableStore;
+
+    // Setup
+    await uncheckedResult(initApp(trace));
 
     // Input
     const rl = readline.createInterface({ input, output });
@@ -25,16 +37,44 @@ const main = makeAsyncResultFunc(
       if (credInput !== '') {
         const password = await rl.question('Enter password for credential: ');
 
-        const result = await decryptEmailCredentialWithPassword(trace, {
+        const credsResult = await decryptEmailCredentialWithPassword(trace, {
           encryptedEmailCredential: credInput as Base64String,
           password
         });
-        if (!result.ok) {
-          console.error('Failed to decrypt credential:', result.value);
+        if (!credsResult.ok) {
+          console.error('Failed to decrypt credential:', credsResult.value);
           process.exit(1);
         }
 
-        creds = result.value;
+        const creds = credsResult.value;
+        const storeResult = await getServerSyncableStore(trace, {
+          storageRootId: storageRootIdInfo.make(creds.userId),
+          userKeys: {
+            getPrivateCryptoKeySetIds: makeAsyncResultFunc(
+              [import.meta.filename, 'getPrivateCryptoKeySetIds'],
+              async (_trace): PR<CryptoKeySetId[]> => makeSuccess([creds.privateKeys.id])
+            ),
+
+            getPrivateCryptoKeySet: makeAsyncResultFunc(
+              [import.meta.filename, 'getPrivateCryptoKeySet'],
+              async (trace, id?: CryptoKeySetId): PR<PrivateCombinationCryptoKeySet, 'not-found'> => {
+                if (id === undefined || id === creds.privateKeys.id) {
+                  return makeSuccess(creds.privateKeys);
+                }
+
+                return makeFailure(new NotFoundError(trace, { message: `No signing key found with ID: ${id}`, errorCode: 'not-found' }));
+              }
+            )
+          } satisfies UserKeys,
+          saltsById: creds.saltsById,
+          creatorPublicKeys: creds.privateKeys.publicOnly()
+        });
+        if (!storeResult.ok) {
+          console.error('Failed to get store:', storeResult.value);
+          process.exit(1);
+        }
+
+        store = storeResult.value;
       } else {
         // TODO:
         // const result = await getMailAgentUserKeys(trace);
@@ -50,8 +90,9 @@ const main = makeAsyncResultFunc(
       rl.close();
     }
 
-    // Placeholder for next step logic
-    console.log('cli-inspect-store main() executed', creds);
+    // Expose
+    await prettyStoreLs(store);
+
     return makeSuccess(undefined);
   },
   {
