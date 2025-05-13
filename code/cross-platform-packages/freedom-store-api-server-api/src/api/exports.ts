ac3cca308fbbd26fd2fde5e92642f1c3c80bf571
Pavel Koryagin <pavel.koryagin@freedomtechhq.com>
2025-05-13 22:34:38 +0200

    lint

--- code/cross-platform-packages/freedom-store-api-server-api/src/api/exports.ts
+++ code/cross-platform-packages/freedom-store-api-server-api/src/api/exports.ts
@@ -4,5 +4,5 @@ export * as publicKeys from './publicKeys.ts';
 export * as pull from './pull.ts';
 export * as push from './push.ts';
 export * as register from './register.ts';
-export * as storeCredentials from './store-credentials.ts';
 export * as retrieveCredentials from './retrieve-credentials.ts';
+export * as storeCredentials from './store-credentials.ts';
