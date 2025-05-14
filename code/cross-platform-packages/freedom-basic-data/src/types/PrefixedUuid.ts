7283119fb1b9078ef93f35126933055c03796044
Brian Westphal <brian@freedomtechhq.com>
2025-05-14 15:52:31 +0800

    More Id Helper Functions - Exposed a few more helper functions for PrefixedTimeId, PrefixedUuid, TimeId, and Uuid

--- code/cross-platform-packages/freedom-basic-data/src/types/PrefixedUuid.ts
+++ code/cross-platform-packages/freedom-basic-data/src/types/PrefixedUuid.ts
@@ -14,6 +14,17 @@ export const makePrefixedUuidSchema = <PrefixT extends `${string}_`>(prefix: Pre
   }).setAllowedLengthRange(prefix.length + UUID_LENGTH, prefix.length + UUID_LENGTH);
 export type PrefixedUuid<PrefixT extends `${string}_`> = `${PrefixT}${Uuid}`;
 
+const tailAnchoredUuidRegex = new RegExp(`_(${nonAnchoredUuidRegex.source})$`);
+export const isPrefixedUuid = (value: string): value is PrefixedUuid<`${string}_`> => tailAnchoredUuidRegex.test(value);
+export const extractUuidFromPrefixedUuid = (prefixedUuid: PrefixedUuid<`${string}_`>): Uuid => {
+  const match = tailAnchoredUuidRegex.exec(prefixedUuid);
+  if (match === null) {
+    throw new Error(`Invalid PrefixedUuid encountered`);
+  }
+
+  return match[1] as Uuid;
+};
+
 export const nonAnchoredAnyPrefixUuidRegex = new RegExp(`[^]*_${nonAnchoredUuidRegex.source}`);
 export const anyPrefixedUuidSchema = schema.regex<PrefixedUuid<`${string}_`>>(new RegExp(`^${nonAnchoredAnyPrefixUuidRegex.source}$`));
 export type AnyPrefixedUuid = typeof anyPrefixedUuidSchema.valueType;
