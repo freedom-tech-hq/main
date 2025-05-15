64cb9938d747a909fc016736f5e9767a4b32c8df
Pavel Koryagin <pavel.koryagin@freedomtechhq.com>
2025-05-15 21:48:32 +0200

    Locking placeholder

--- code/cross-platform-packages/freedom-syncable-store/src/internal/types/DefaultMutableSyncableItemAccessorBase.ts
+++ code/cross-platform-packages/freedom-syncable-store/src/internal/types/DefaultMutableSyncableItemAccessorBase.ts
@@ -1,6 +1,7 @@
 import type { PR, PRFunc, Result } from 'freedom-async';
 import { makeAsyncResultFunc, makeFailure, makeSuccess } from 'freedom-async';
 import { generalizeFailureResult, InternalStateError } from 'freedom-common-errors';
+import { InMemoryLockStore, withAcquiredLock } from 'freedom-locking-types';
 import type { DynamicSyncableItemName, LocalItemMetadata, SyncableItemMetadata, SyncablePath } from 'freedom-sync-types';
 import { extractSyncableItemTypeFromPath, isCompleteLocalItemMetadata } from 'freedom-sync-types';
 import type { SyncableStoreBacking } from 'freedom-syncable-store-backing-types';
@@ -11,6 +12,8 @@ import { getNearestFolderPath } from '../../utils/get/getNearestFolderPath.ts';
 import { markSyncableNeedsRecomputeLocalMetadataAtPath } from '../utils/markSyncableNeedsRecomputeLocalMetadataAtPath.ts';
 import { DefaultMutableSyncableFolderAccessor } from './DefaultMutableSyncableFolderAccessor.ts';
 import type { FolderOperationsHandler } from './FolderOperationsHandler.ts';
+
+const tempLockStore = new InMemoryLockStore();
 
 export interface DefaultMutableSyncableItemAccessorBaseConstructorArgs {
   backing: SyncableStoreBacking;
@@ -121,18 +124,29 @@ export abstract class DefaultMutableSyncableItemAccessorBase implements MutableS
   public readonly markNeedsRecomputeLocalMetadata = makeAsyncResultFunc(
     [import.meta.filename, 'markNeedsRecomputeLocalMetadata'],
     async (trace): PR<undefined> => {
-      const updatedHash = await this.backing_.updateLocalMetadataAtPath(trace, this.path, {
-        hash: undefined,
-        numDescendants: 0,
-        sizeBytes: 0
-      });
-      if (!updatedHash.ok) {
-        return generalizeFailureResult(trace, updatedHash, ['not-found', 'wrong-type']);
+      const store = this.weakStore_.deref();
+      if (store === undefined) {
+        return makeFailure(new InternalStateError(trace, { message: 'store was released' }));
       }
 
-      this.needsRecomputeLocalMetadataCount_ += 1;
+      const updated = await withAcquiredLock(trace, tempLockStore.lock(store.uid), {}, async (trace) => {
+        const updatedHash = await this.backing_.updateLocalMetadataAtPath(trace, this.path, {
+          hash: undefined,
+          numDescendants: 0,
+          sizeBytes: 0
+        });
+        if (!updatedHash.ok) {
+          return generalizeFailureResult(trace, updatedHash, ['not-found', 'wrong-type']);
+        }
 
-      const store = this.weakStore_.deref();
+        this.needsRecomputeLocalMetadataCount_ += 1;
+
+        return makeSuccess(undefined);
+      });
+      if (!updated.ok) {
+        return generalizeFailureResult(trace, updated, 'lock-timeout');
+      }
+
       const parentPath = this.path.parentPath;
       if (store !== undefined && parentPath !== undefined) {
         const marked = await markSyncableNeedsRecomputeLocalMetadataAtPath(trace, store, parentPath);
@@ -152,25 +166,31 @@ export abstract class DefaultMutableSyncableItemAccessorBase implements MutableS
   private readonly computeLocalItemMetadataAndUpdateBacking_ = makeAsyncResultFunc(
     [import.meta.filename, 'computeLocalItemMetadataAndUpdateBacking_'],
     async (trace): PR<LocalItemMetadata> => {
-      DEV: this.weakStore_.deref()?.devLogging.appendLogEntry?.({ type: 'get-metadata', pathString: this.path.toString() });
+      const store = this.weakStore_.deref();
+      if (store === undefined) {
+        return makeFailure(new InternalStateError(trace, { message: 'store was released' }));
+      }
 
-      do {
-        const needsRecomputeLocalMetadataCount = this.needsRecomputeLocalMetadataCount_;
+      DEV: store.devLogging.appendLogEntry?.({ type: 'get-metadata', pathString: this.path.toString() });
 
+      const updated = await withAcquiredLock(trace, tempLockStore.lock(store.uid), {}, async (trace) => {
         const localItemMetadata = await this.computeLocalItemMetadata_(trace);
         if (!localItemMetadata.ok) {
           return localItemMetadata;
         }
 
-        if (this.needsRecomputeLocalMetadataCount_ === needsRecomputeLocalMetadataCount) {
-          const updatedHash = await this.backing_.updateLocalMetadataAtPath(trace, this.path, localItemMetadata.value);
-          if (!updatedHash.ok) {
-            return generalizeFailureResult(trace, updatedHash, ['not-found', 'wrong-type']);
-          }
-
-          return makeSuccess(localItemMetadata.value);
+        const updatedHash = await this.backing_.updateLocalMetadataAtPath(trace, this.path, localItemMetadata.value);
+        if (!updatedHash.ok) {
+          return generalizeFailureResult(trace, updatedHash, ['not-found', 'wrong-type']);
         }
-      } while (true);
+
+        return makeSuccess(localItemMetadata.value);
+      });
+      if (!updated.ok) {
+        return generalizeFailureResult(trace, updated, 'lock-timeout');
+      }
+
+      return makeSuccess(updated.value);
     }
   );
 }
