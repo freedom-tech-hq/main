efb25a09947179497aedf08bf1d89058c24b49a7
Pavel Koryagin <pavel.koryagin@freedomtechhq.com>
2025-05-15 13:39:45 +0200

    Updated dev email domain

--- code/apps/freedom-email-app/src/task-worker-configs/dev.ts
+++ code/apps/freedom-email-app/src/task-worker-configs/dev.ts
@@ -1,6 +1,6 @@
 import type { EmailTasksWebWorkerConfig } from 'freedom-email-tasks-web-worker';
 
 export const dev: EmailTasksWebWorkerConfig = {
-  defaultEmailDomain: 'dev.linefeedr.com',
+  defaultEmailDomain: 'mail-host.dev.linefeedr.com',
   mailApiServerBaseUrl: 'https://api.dev.linefeedr.com'
 };
