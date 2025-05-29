c0749163db97ba19ed15580dd41446102d747e6f
Pavel Koryagin <pavel.koryagin@freedomtechhq.com>
2025-05-29 22:55:25 +0200

    Revise for User entity

--- code/server-packages/freedom-db/src/model/utils/addUser.ts
+++ code/server-packages/freedom-db/src/model/utils/addUser.ts
@@ -1,28 +1,21 @@
 import { makeAsyncResultFunc, makeFailure, makeSuccess, type PR } from 'freedom-async';
-import { ConflictError, InternalStateError } from 'freedom-common-errors';
+import { ConflictError } from 'freedom-common-errors';
 import { isEqual } from 'lodash-es';
 import { DatabaseError } from 'pg';
 
 import { dbQuery } from '../../db/postgresClient.ts';
-import type { RawUser } from '../internal/types/RawUser.ts';
-import { type User, userSchema } from '../types/User.ts';
+import { getRawUserFromUser, type RawUser } from '../internal/types/RawUser.ts';
+import type { User } from '../types/User.ts';
 
 type ErrorCodes = 'already-created' | 'conflict' | 'email-unavailable';
 
 export const addUser = makeAsyncResultFunc([import.meta.filename, 'addUser'], async (trace, user: User): PR<User, ErrorCodes> => {
   // Validate and convert
-  const serialization = await userSchema.serializeAsync(user);
-  if (serialization.error !== undefined) {
-    return makeFailure(
-      new InternalStateError(trace, {
-        message: `Incorrect user data: ${serialization.error}`
-      })
-    );
+  const rawUserResult = await getRawUserFromUser(trace, user);
+  if (!rawUserResult.ok) {
+    return rawUserResult;
   }
-
-  // TODO: Study typing problem for serializing
-  const rawUser = serialization.serialized as unknown as RawUser;
-  rawUser.encryptedCredentials = rawUser.encryptedCredentials ?? null;
+  const rawUser = rawUserResult.value;
 
   try {
     await dbQuery(
