1154d94a46b26a48d4eda350b570215f2bd3c58b
Pavel Koryagin <pavel.koryagin@freedomtechhq.com>
2025-06-01 12:58:54 +0200

    stash

---
+++ code/backends/freedom-mail-host/src/modules/formats/internal/utils/convertMailAddress.ts
@@ -0,0 +1,37 @@
+import type { MailAddress, MailAddressList } from 'freedom-email-sync';
+import type { EmailAddress } from 'mailparser';
+
+export function convertMailAddress(list: EmailAddress[]): MailAddressList {
+  const result: MailAddressList = [];
+
+  for (const addr of list) {
+    if (addr.address !== undefined) {
+      result.push({
+        name: addr.name,
+        address: addr.address
+      });
+    }
+
+    // address and group are mutually exclusive. Typing of 'mailparser' is imperfect
+    if (addr.group !== undefined) {
+      const addresses: MailAddress[] = [];
+
+      for (const item of addr.group) {
+        if (item.address !== undefined) {
+          addresses.push({
+            name: item.name,
+            address: item.address
+          });
+        }
+        // else should not happen TODO: log
+      }
+
+      result.push({
+        groupName: addr.name,
+        addresses
+      });
+    }
+  }
+
+  return result;
+}
