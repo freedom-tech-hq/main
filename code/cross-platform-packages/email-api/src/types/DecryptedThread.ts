e47dcb408f6383307338db2e89476266de1b4d2e
Pavel Koryagin <pavel.koryagin@freedomtechhq.com>
2025-06-01 16:31:15 +0200

    stash

--- code/cross-platform-packages/email-api/src/types/DecryptedThread.ts
+++ code/cross-platform-packages/email-api/src/types/DecryptedThread.ts
@@ -1,27 +1,14 @@
-import { isoDateTimeSchema } from 'freedom-basic-data';
-import { schema } from 'yaschema';
-
-export const decryptedThreadPartSchema = schema.object({
-  subject: schema.string(),
-  snippet: schema.string()
-});
+import type { DecryptedListMessage } from './DecryptedListMessage.ts';
 
 // Note: thread does not have list and view variants.
 // It is always a list version, while the view is a list of ViewMessage.
-export const decryptedThreadSchema = schema.object({
+export interface DecryptedThread {
   // ### Open fields ###
-  id: schema.string(),
-  // assumed // userId,
+  id: string;
+  // assumed // userId
+  // assumed // folder
 
   // ### Dynamic ###
-  // Do we need this // messageIds: schema.array(schema.string()),
-  messageCount: schema.number(),
-
-  // ### Dynamic, of the last (unread?) message, see DecryptedListMessage ###
-  transferredAt: isoDateTimeSchema,
-  subject: schema.string().allowEmptyString(),
-  snippet: schema.string().allowEmptyString(), // always plain text, single line, trimmed at 100 chars
-  hasAttachments: schema.boolean() // From the same message as listMessage
-});
-
-export type DecryptedThread = typeof decryptedThreadSchema.valueType;
+  messageCount: number;
+  lastUnreadMessage: DecryptedListMessage;
+}
