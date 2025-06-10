dc78dd5f454d41129b12749b89ab8bc95c1f70fe
Pavel Koryagin <pavel.koryagin@freedomtechhq.com>
2025-06-10 13:39:52 +0200

    Delete lost freedom-syncable-store-server usage

--- code/backends/freedom-store-api-server/src/initApp.ts
+++ code/backends/freedom-store-api-server/src/initApp.ts
@@ -3,7 +3,6 @@ import fs from 'node:fs';
 import type { PR } from 'freedom-async';
 import { makeAsyncResultFunc, makeSuccess } from 'freedom-async';
 import { initConfig as dbInitConfig } from 'freedom-db';
-import { initConfig as syncableStoreServerInitConfig } from 'freedom-syncable-store-server';
 
 import * as rawConfig from './config.ts';
 
@@ -21,7 +20,6 @@ export const initApp = makeAsyncResultFunc([import.meta.filename], async (_trace
 
   // Modules
   dbInitConfig(config);
-  syncableStoreServerInitConfig(config);
 
   return makeSuccess(undefined);
 });
